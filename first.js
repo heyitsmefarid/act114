5b396669274e57b6b42f2d14d167b95c40013393
heyitsmefarid <gallanofredanthony06@gmail.com>
2024-06-06 16:33:12 +0800

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    document.getElementById('zel').style.color= "red";
+    document.getElementById('zel').style.fontStyle ="inherit"
+}
+function changeimage(){
+    document.getElementById('farid').src = "images/hiii.gif";
+}

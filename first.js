8d2a1467a2f7d6b64277549dd1d1fb992a0fdc90
heyitsmefarid <gallanofredanthony06@gmail.com>
2024-06-06 16:31:11 +0800

    Delete first.js

--- first.js
+++
@@ -1,7 +0,0 @@
-function changecolor(){
-    document.getElementById('zel').style.color= "red";
-    document.getElementById('zel').style.fontStyle ="inherit"
-}
-function changeimage(){
-    document.getElementById('farid').src = "images/hiii.gif";
-}
